c53511ef46e6169e6fa9c33024fece10964e3456
Habib Aroua <HabibAroua@users.noreply.github.com>
2021-07-05 09:28:43 +0100

    dev the class Event

--- app/Events/ChatEvent.php
+++ app/Events/ChatEvent.php
@@ -1,36 +1,31 @@
 <?php
+	namespace App\Events;
 
-namespace App\Events;
+	use App\Chat;
+	use App\Message;
+	use Illuminate\Broadcasting\Channel;
+	use Illuminate\Queue\SerializesModels;
+	use Illuminate\Broadcasting\PrivateChannel;
+	use Illuminate\Broadcasting\PresenceChannel;
+	use Illuminate\Foundation\Events\Dispatchable;
+	use Illuminate\Broadcasting\InteractsWithSockets;
+	use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
-use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
+	class ChatEvent implements ShouldBroadcast
+	{
+		use Dispatchable, InteractsWithSockets, SerializesModels;
 
-class ChatEvent
-{
-    use Dispatchable, InteractsWithSockets, SerializesModels;
+		public $chat;
 
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
+		public function __construct(Message $chat)
+		{
+			$this->chat = $chat;
+		}
 
-    /**
-     * Get the channels the event should broadcast on.
-     *
-     * @return \Illuminate\Broadcasting\Channel|array
-     */
-    public function broadcastOn()
-    {
-        return new PrivateChannel('channel-name');
-    }
-}
+		public function broadcastOn()
+		{
+			return new PresenceChannel('chat');
+		}
+	}
+
+?>
